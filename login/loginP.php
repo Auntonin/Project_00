5284895ea77cfd2775dc530bd31ba3b96e6c20f0
autonin <116173491+ninjatapza@users.noreply.github.com>
2022-12-06 00:22:28 +0700

    1 login and adduser

--- login/loginP.php
+++ login/loginP.php
@@ -1,40 +1,30 @@
 <?php
 session_start();
 require_once "../condb.php";
-$uname=$_POST['usernames'];
-$upasswd=$_POST['Password'];
-$_SESSION['login_name']="";
+$uname = $_POST['usernames'];
+$upasswd = $_POST['Password'];
+$_SESSION['login_name'] = "";
 
 
-$sql="SELECT * FROM user WHERE user_name='$uname' AND user_password='$upasswd' ";
+$sql = "SELECT * FROM user WHERE user_name='$uname' AND user_password='$upasswd' ";
 
-$result=$conn->query($sql);
-if($result->num_rows == 1)
-{
-    $rs=$result->fetch_array();
-    $_SESSION['user_id']=(int)$rs['user_id'];
-    $_SESSION['user_level']=(int)$rs['user_level'];
-    $_SESSION['login_name']=$rs['user_name'] ;
-    $_SESSION['full_name']=$rs['user_firstname']." ".$rs['user_lastname'];
+$result = $conn->query($sql);
+if ($result->num_rows == 1) {
+    $rs = $result->fetch_array();
+    $_SESSION['user_id'] = (int) $rs['user_id'];
+    $_SESSION['user_level'] = (int) $rs['user_level'];
+    $_SESSION['login_name'] = $rs['user_name'];
+    $_SESSION['full_name'] = $rs['user_firstname'] . " " . $rs['user_lastname'];
 
-    if($rs['user_level'] == 1)
-    {
+    if ($rs['user_level'] == 1) {
         header("location: ../index.php");
-    }
-    
-    else if($rs['user_level'] == 0)
-    {
+    } else if ($rs['user_level'] == 0) {
         header("location: ../admin.php");
-    }
-    
-    else
-    {
+    } else {
         header("location: login.php");
     }
-    
-}
-else
-{
+
+} else {
     echo "No!";
     header("location: login.php");
 }
