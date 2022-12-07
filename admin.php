1fb4ecda116198853f7e9893d71d1ab81b228464
autnonin <autonin@gmail.com>
2022-12-07 03:00:23 -0800

    +product

--- admin.php
+++ admin.php
@@ -23,20 +23,15 @@ if (isset($_SESSION['user_level']) && $_SESSION['user_level'] == 0) {
   require_once("manu.php")
       ?>
     <h1>admin</h1>
-    <table align="center" border="3" class="border border-secondary">
+    <table align="center" border="1" class="border border-secondary">
       <tr>
-
         <td>category</td>
       </tr>
-
-
       <?php
   $sql = "SELECT * FROM category ORDER BY cate_name";
   $result = $conn->query($sql);
   while ($rs = $result->fetch_array()) {
       ?>
-
-
       <td>
         <?= $cid=$rs['cate_id']; ?>
       </td>
@@ -46,27 +41,52 @@ if (isset($_SESSION['user_level']) && $_SESSION['user_level'] == 0) {
 
       <td>
         <?php echo "<a type='button' class='btn btn-outline-primary me-2' href='admin/edit_cate.php?cate_id=$cid'>edit</a></td>"; ?>
-     
+
       </td>
-
-      
-
-
-
       </tr>
       <?php
   }
-
-
       ?>
       <tr>
-        <td><a type='button' class='btn btn-outline-primary me-2' href='admin/add_cate.php'>add-product</a></td>
+        <td><a type='button' class='btn btn-outline-primary me-2' href='admin/add_cate.php'>add-category</a></td>
+      </tr>
+    </table>
+
+    <table align="center" border="1" class="border border-secondary">
+      <tr>
+        <td>products</td>
+      </tr>
+      <?php
+  $sql = "SELECT * FROM products ORDER BY product_name";
+  $result = $conn->query($sql);
+  while ($rs = $result->fetch_array()) {
+      ?>
+      <td>
+        <?= $pid=$rs['product_id']; ?>
+      </td>
+      <td>
+        <?= $cid=$rs['cate_id']; ?>
+      </td>
+      <td>
+        <?= $pn=$rs['product_name']; ?>
+      </td>
+
+      <td>
+        <?php echo "<a type='button' class='btn btn-outline-primary me-2' href='admin/edit_cate.php?product_id=$pid'>edit</a></td>"; ?>
+
+      </td>
+      </tr>
+      <?php
+  }
+      ?>
+      <tr>
+        <td><a type='button' class='btn btn-outline-primary me-2' href='admin/add_cate.php'>add-category</a></td>
       </tr>
     </table>
   </div>
 
 
-  </div>
+
 
   <!-- JavaScript Bundle with Popper -->
   <script src="bootstrap/js/bootstrap.min.js"></script>
---
+++ admin/add_product.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+if(isset($_SESSION['user_level']) && $_SESSION['user_level']==0)
+{
+    require_once('../condb.php');
+    if(isset($_POST['product_name']) && trim($_POST['product_name']) != "")
+    {
+        $cate_id = $_POST['cate_id'];
+        $product_qty=$_POST['product_qty'];
+        $product_price=$_POST['product_price'];
+        $sql="SELECT product_name FROM products WHERE product_name = '".trim($_POST['product_name'])."'";
+      
+        $result=$conn->query($sql);
+        if($result->num_rows==0)
+        {
+            $sql="INSERT INTO products VALUES(0,$cate_id,'".trim($_POST['product_name'])."',".$product_price.",".$product_qty.")";
+           $result=$conn->query($sql);
+          
+        alert('OK\nสำเร็จ');
+            header("location:../admin.php");
+        }
+        else
+        {
+            alert("เฮ้ย! ชื่อประเภทสินค้ามีอยู่แล้ว");
+        }
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link href="../bootstrap/css/signin.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+
+        <form class="form " action="" method="post">
+            <div class="form-inline">
+                <label for="cate_id">ประเภทสินค้า</label>
+                <select name="cate_id" id="cate_id">
+                    <?php
+                    $sql = "SELECT * FROM category";
+    $result = $conn->query($sql);
+    while($rs=$result->fetch_array())
+    {
+        echo "<option value='".$rs['cate_id']."'>".$rs['cate_name']."</option>";
+    }
+                    ?>
+                
+                </select>
+                <input type="text" name="product_name" require>
+                <input type="number" name="product_price" min="1" max="1000" require>
+                <input type="number" name="product_qty" min="1" require>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit" value="ok">Add</button>
+            <br>
+        </form>
+        <?php
+        $sql="SELECT * FROM category ORDER BY cate_name";
+        $result=$conn->query($sql);
+        while($rs=$result->fetch_array())
+        {
+            $cid=$rs['cate_id'];
+            $cn=$rs['cate_name'];
+            echo "<br>$cid ==> <a href='edit_cate.php?cate_id=$cid'>$cn</a>";
+        }       
+        ?>
+
+    </div>
+</body>
+
+</html>
+<?php
+}
+else{
+    header('location: ../index.php');
+
+}
+?>
