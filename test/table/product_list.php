9c85679db82f56956c503bd84cc667d9c44922a1
autonin <116173491+ninjatapza@users.noreply.github.com>
2023-01-23 23:13:53 +0700

    ajax_post

--- test/table/product_list.php
+++ test/table/product_list.php
@@ -1,11 +1,12 @@
 <?php
 include_once '../../condb.php';
-header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
-header('Content-Type: application/json; charset=utf-8');
-    // print_r($_POST);
-if(isset($_POST['cate']) && $_POST['cate'] != '')
+// header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+// header('Content-Type: application/json; charset=utf-8');
+    print_r($_POST);
+//  echo $_POST['cate'] ;
+if(isset($_POST['5']) && $_POST['5'] != '')
 {
-    $cate_id = trim($_REQUEST['cate']);
+    $cate_id = trim($_REQUEST['5']);
     // if($cate_id == "all"){
     //     $query = "SELECT p.*,c.cate_name
     //     FROM products p INNER JOIN category c 
@@ -20,7 +21,7 @@ if(isset($_POST['cate']) && $_POST['cate'] != '')
     //     WHERE p.cate_id='$cate_id'
     //     ORDER BY product_name";
     // }
-    $query = "SELECT  * FROM products WHERE cate_id=1";
+    $query = "SELECT  * FROM products WHERE cate_id= $cate_id";
     $result = $conn->query($query);
     if ($result) 
     {
@@ -31,7 +32,7 @@ if(isset($_POST['cate']) && $_POST['cate'] != '')
         }     
     }
     $datax = array('data' => $data);
-    echo json_encode($datax, JSON_UNESCAPED_UNICODE);
+    echo json_encode($datax,JSON_UNESCAPED_UNICODE);
 
 } else {
     echo "can't query";
--- test/table/table.php
+++ test/table/table.php
@@ -16,18 +16,20 @@
 <body>
   <div class="container">
     <h3>ประเภทสินค้า</h3>
-    <select name="cate" id="cate" class="mb-5">
-      <option value="all">เลือก</option>
-      <?php
-      require_once('../../condb.php');
-      $sql = "SELECT * FROM category";
-      $result = $conn->query($sql);
-      while ($rs = $result->fetch_array()) {
-        echo "<option value='" . $rs['cate_id'] . "'>" . $rs['cate_name'] . "</option>";
-      }
-      ?>
-    </select>
+    <form action="" id="formMain" method="post">
+      <select name="cate" id="cate" class="mb-5"> 
+        <option value="all">เลือก</option>
+        <?php
+        require_once('../../condb.php');
+        $sql = "SELECT * FROM category";
+        $result = $conn->query($sql);
+        while ($rs = $result->fetch_array()) {
+          echo "<option name='cate' value='" . $rs['cate_id'] . "'>" . $rs['cate_name'] . "</option>";
+        }
+        ?>
+      </select>
 
+    </form>
     <div class="table-responsive">
       <table id="product_list" class="table table-bordered table-striped">
         <thead>
@@ -57,60 +59,67 @@
 
   </div>
   <script>
-   $(document).ready(function () {
-    // table = $('#product_list').DataTable();
-    $('#cate').change(function () {
-    
-      var url = 'product_list.php'; // ระบุไฟล์ที่ต้องการให้ส่งข้อมูลไปเพื่อตอบกลับมา
-      table = $('#product_list').DataTable({
-        "destroy": true,
-        "paging": true,
-        "lengthChange": true,
-        "processing": true,
-        //"serverSide": true,
-        "searching": true,
-        "ordering": true,
-        "info": true,
-        "responsive": true,
-        "autoWidth": false,
-        "pageLength": 25,
-        // ส่งข้อมูล แบบ POST ไปยังไฟล์ ที่กำหนดไว้ใน url
-        "ajax": {
-          "url": url,
-          "type": "POST",
-          "data": function (d) { // กำหนดค่าให้ตัวแปร data คือค่าที่ได้มาจากการส่งค่าไป
-            d.business_name = $('#business_name').val();
+    $(document).ready(function () {
+      // table = $('#product_list').DataTable();
+      $('#cate').change(function () {
+        // var cid=$(this).attr("id");
+        var url = 'product_list.php'; // ระบุไฟล์ที่ต้องการให้ส่งข้อมูลไปเพื่อตอบกลับมา
+        table = $('#product_list').DataTable({
+          "destroy": true,
+          "paging": true,
+          "lengthChange": true,
+          "processing": true,
+          //"serverSide": true,
+          "searching": true,
+          "ordering": true,
+          "info": true,
+          "responsive": true,
+          "autoWidth": false,
+          "pageLength": 25,
+          // ส่งข้อมูล แบบ POST ไปยังไฟล์ ที่กำหนดไว้ใน url
+          
+          "ajax": {
+            "url":url,
+            "type":"POST",
+            "data":  
+            // {id:cid},
+            $("#cate").serialize(),
+           function (data) { // กำหนดค่าให้ตัวแปร data คือค่าที่ได้มาจากการส่งค่าไป
+              // d.business_name = $('#business_name').val()
+              console.log(data);
+           
+            }
 
+
+          },
+          "columns": [
+            { "data": "product_id" },
+            { "data": "cate_id" },
+            { "data": "product_name" },
+            { "data": "product_price" },
+            { "data": "product_qty" },
+            { "data": "product_images" },
+          ],
+          "language": {
+            "processing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">กำลังค้นหาข้อมูล...</span> ',
+            "lengthMenu": "แสดง _MENU_ แถวต่อหน้า",
+            "zeroRecords": "ไม่มีข้อมูล",
+            "info": "กำลังแสดงข้อมูล _START_ ถึง _END_ จาก _TOTAL_ รายการ",
+            "search": "ค้นหา:",
+            "infoEmpty": "ไม่มีข้อมูลแสดง",
+            "infoFiltered": "(ค้นหาจาก _MAX_ total records)",
+            "paginate": {
+              "first": "หน้าแรก",
+              "last": "หน้าสุดท้าย",
+              "next": "หน้าต่อไป",
+              "previous": "หน้าก่อน"
+            }
           }
-        },
-        "columns": [
-          { "data": "product_id" },
-          { "data": "cate_id" },
-          { "data": "product_name" },
-          { "data": "product_price" },
-          { "data": "product_qty" },
-          { "data": "product_image" },
-        ],
-        "language": {
-          "processing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">กำลังค้นหาข้อมูล...</span> ',
-          "lengthMenu": "แสดง _MENU_ แถวต่อหน้า", 
-          "zeroRecords": "ไม่มีข้อมูล",
-          "info": "กำลังแสดงข้อมูล _START_ ถึง _END_ จาก _TOTAL_ รายการ",
-          "search": "ค้นหา:",
-          "infoEmpty": "ไม่มีข้อมูลแสดง",
-          "infoFiltered": "(ค้นหาจาก _MAX_ total records)",
-          "paginate": {
-            "first": "หน้าแรก",
-            "last": "หน้าสุดท้าย",
-            "next": "หน้าต่อไป",
-            "previous": "หน้าก่อน"
-          }
-        }
+        });
+        return false;
       });
-      return false;
-    });
 
-  });
+    });
 
   </script>
 </body>
